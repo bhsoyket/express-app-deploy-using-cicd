ef6c5dc694bf2fdc5234d46e1725db1858072bbe
Belayet Hossain <2020belayethossain@gmail.com>
2025-01-31 00:18:38 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`server listening on http://127.0.0.1:${PORT}`);
+});
+
+module.exports = app;
